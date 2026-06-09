bf43945a205a08ec711df49c7fce034f972caccd
Surya <suryaajis330@gmail.com>
2026-06-09 12:17:09 +0700

    Remove Credential Dummy

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/v1/:path*`,
       },
     ];
   },
